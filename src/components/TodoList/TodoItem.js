9df626cef2ae75bda32e831d814940fe19e3c9b5
wangxin <wangxin@163.com>
2020-04-22 13:45:30 +0800

    生命周期判断是不是跟新

--- src/components/TodoList/TodoItem.js
+++ src/components/TodoList/TodoItem.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-
+// PureComponent 数据浅比较
 class TodoItem extends Component {
-    state = {  }
+    constructor(){
+        super()
+        this.state = {}
+    }
     handleCheckboxChange = () => {
         // 判断有没有再执行
         // this.props.onCompeletedChange && this.props.onCompeletedChange(this.props.id)   
@@ -11,6 +14,17 @@ class TodoItem extends Component {
         }  = this.props
         onCompeletedChange(id)
     }
+
+    // 生命周期
+    shouldComponentUpdate(nextProps, nextState) { // 会重复渲染返回true 或者 false ,当两个值不相等的时候渲染
+        // return nextProps.isCompleted !== this.props.isCompleted
+    } 
+
+    // UNSAFE_componentWillReceiveProps(nextProps){
+    //     this.setState({
+    //         completedText: nextProps.isCompleted ? '已完成' : '未完成'
+    //     })
+    // }
     render() { 
         return ( 
             <li>
